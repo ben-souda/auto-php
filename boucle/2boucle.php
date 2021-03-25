8321f96c071aabb29cab736f3fa82a6f83e9a05c
ben-souda <76470837+ben-souda@users.noreply.github.com>
2021-03-25 15:33:24 +0100

    Add files via upload
    hi

---
+++ boucle/2boucle.php
@@ -0,0 +1,9 @@
+<?php
+$repetitions = 0;
+
+for ($repetitions = 0 ; $repetitions < 10 ; $repetitions++)
+{
+    echo '<p>Je ne doit pas copier sur mon voisin. ' . $repetitions . 'fois.</p>';
+}
+
+?>
